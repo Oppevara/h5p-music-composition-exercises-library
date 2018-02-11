198b08d822bd144791be02ccb1c61471088c134f
Pjotr Savitski <gnum@tlu.ee>
2018-02-11 22:34:07 +0200

    References Oppevara/oppevara#61, attempts and dependencies.
    Adde logic for setting maxQuestions (attempts) value for the test.
    Changed the HTML part to allow changing the default value. Moved all the
    dependencies as local loaded files (there seem to have been issues whne
    loading in any other way).

--- scripts/index.js
+++ scripts/index.js
@@ -2,40 +2,6 @@
  * Music Composition Exercises Editor library
  */
 
- // It would be useful to load those as H5P dependencies, but those seem to want to attach themselves to global window object
- (function(){
-   if (window.VEXTAB_LOADED === true) return;
-   window.VEXTAB_LOADED = true;
-   var script = document.createElement("script");
-   // XXX This should be loaded from here: https://unpkg.com/vextab@2.0.13/releases/vextab-div.js
-   script.src = "https://unpkg.com/vextab/releases/vextab-div.js";
-   document.head.appendChild(script);
- })();
- (function(){
-   if (window.WEB_AUDIO_FONT_PLAYER_LOADED === true) return;
-   window.WEB_AUDIO_FONT_PLAYER_LOADED = true;
-   var script = document.createElement("script");
-   // XXX This should either become part of the library or come from CDN
-   script.src = "https://surikov.github.io/webaudiofont/npm/dist/WebAudioFontPlayer.js";
-   document.head.appendChild(script);
- })();
- (function(){
-   if (window.WEB_AUDIO_SOND_LOADED === true) return;
-   window.WEB_AUDIO_SOND_LOADED = true;
-   var script = document.createElement("script");
-   // XXX This should either become part of the library or come from CDN
-   script.src = "https://surikov.github.io/webaudiofontdata/sound/0000_JCLive_sf2_file.js";
-   document.head.appendChild(script);
- })();
- (function(){
-   if (window.JSPDF_LOADED === true) return;
-   window.JSPDF_LOADED = true;
-   var script = document.createElement("script");
-   // XXX This should either become part of the library or come from CDN
-   script.src = "https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.5/jspdf.min.js";
-   document.head.appendChild(script);
- })();
-
 var H5P = H5P || {};
 
 // XXX This does not seem to get exposed
@@ -312,8 +278,15 @@ H5P.MusicCompositionExercisesLibrary = (function($, JoubelUI) {
     });
     $('<br>').appendTo($testDiv);
     $('<p>').append($('<small>', {
-      'text': 'Teile esitatakse 5 küsimust, mis tuleb ettemääratud aja jooksul vastata. Kui jõuate varem valmis, vajutage lihtsalt "Vasta". Uus küsimus kuvatakse peale eelmise vastamist või aja täitumisel.'
+      'html': 'Teile esitatakse <span class="maxQuestions">5</span> küsimust, mis tuleb ettemääratud aja jooksul vastata. Kui jõuate varem valmis, vajutage lihtsalt "Vasta". Uus küsimus kuvatakse peale eelmise vastamist või aja täitumisel.'
     })).appendTo($testDiv);
+  };
+
+  MusicCompositionExercisesLibrary.setMaxQuestions = function(context, $container, maxQuestions) {
+    if ( context.exercise ) {
+      context.exercise.maxQuestions = maxQuestions;
+      $container.find('span.maxQuestions').text(maxQuestions);
+    }
   };
 
   return MusicCompositionExercisesLibrary;
